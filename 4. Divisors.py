ea443d46538260788458a973a1ded38db782c683
Gamattowicz <p.romanczuk31@gmail.com>
2020-11-08 10:57:37 +0100

    Add whitespace

--- 4. Divisors.py
+++ 4. Divisors.py
@@ -1,6 +1,7 @@
 ''' Python 3.9
 21.10.2020
 
+
 Create a program that asks the user for a number and then prints out
 a list of all the divisors of that number. (If you don’t know what
 a divisor is, it is a number that divides evenly into another number.
