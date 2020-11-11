8be132c3fb1ec01ee1e661398cf1f4687b4ceec5
Gamattowicz <p.romanczuk31@gmail.com>
2020-11-11 21:27:10 +0100

    add descriptions to 6 and 7 exercises

--- 6. String Lists..py
+++ 6. String Lists..py
@@ -1,3 +1,10 @@
+''' Python 3.9
+21.10.2020
+
+Ask the user for a string and print out whether this string is a palindrome or
+not. A palindrome is a string that reads the same forwards and backwards.)'''
+
+
 def reverse(word):
     drow = ' '
     for i in range(len(word)):
